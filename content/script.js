0db0363615412c9a3cad64777694622b03de5f1f
Augustin880 <100044152+Augustin880@users.noreply.github.com>
2023-11-18 20:24:25 +0100

    removing hardcoded data

--- content/script.js
+++ content/script.js
@@ -1,15 +1,6 @@
-// hard coded data : change to data from DataBase later
-var data = [  
-        { description: 'accident de voiture', address: 'rue du bois', from: 'John Dubois', date: '23-05-23' },
-        { description: "chute d'arbre", address: 'rue Albert', from: 'Jean Nemar', date: '25-11-23' },
-        { description: 'trou dans le sol', address: 'rue Puit', from: 'Luc Ogre', date: '09-12-23' },
-        { description: 'voiture accidentée', address: 'rue Grand', from: 'Louis Deschamps', date: '12-02-23' }
-];
-
 // create the submission table for the HTML
-function create_table() {
+function create_table(data) {
     let table = '<table style="width: 90%;"><tr><td>Description</td><td>Adresse</td><td>Rapporté par</td><td>Date</td></tr>';
-    // TODO: should get the data from DataBase
     data.forEach(submission => {
         table += `<tr><td>${submission.description}</td><td>${submission.address}</td><td>${submission.from}</td><td>${submission.date}</td></tr>`;
     });
